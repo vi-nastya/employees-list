9752eeb0cb86538ba2d5b9b5e83718f261c34c7d
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-11-26 19:33:23 +0300

    Refactoring: add AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,15 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import store from './state/store'
-import { Layout } from 'antd'
-import Icon, { GithubFilled } from '@ant-design/icons'
-import styles from './styles.module.css'
-import { Typography } from 'antd'
-const { Title, Link, Text } = Typography
 import { EmployeesList } from './components/EmployeesList'
+import { AppLayout } from './layout/AppLayout'
 
 export const App = (): React.Node => {
-  const { Header, Footer, Content } = Layout
-
   return (
     <Provider store={store}>
-      <Layout className={styles.layout}>
-        <Header className={styles.header}>
-          <Icon
-            type="github"
-            component={() => <GithubFilled style={{ fontSize: 28 }} />}
-            style={{ color: 'white', marginRight: 16 }}
-          />
-          <Title style={{ color: 'white', fontSize: '28px', marginBottom: 0 }}>
-            Employee List
-          </Title>
-        </Header>
-
-        <Content className={styles.content}>
-          <EmployeesList />
-        </Content>
-
-        <Footer className={styles.footer}>
-          <Text>© Anastasia Videneeva</Text>
-          <Link href="https://github.com/vi-nastya" target="_blank">
-            View source code
-          </Link>
-        </Footer>
-      </Layout>
+      <AppLayout>
+        <EmployeesList />
+      </AppLayout>
     </Provider>
   )
 }
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,39 @@
+// @flow
+import * as React from 'react'
+import { Layout } from 'antd'
+import Icon, { GithubFilled } from '@ant-design/icons'
+import styles from './styles.module.css'
+import { Typography } from 'antd'
+const { Title, Link, Text } = Typography
+
+type LayoutProps = {
+  children?: React.Node,
+}
+
+export const AppLayout = (props: LayoutProps): React.Node => {
+  const { Header, Footer, Content } = Layout
+
+  return (
+    <Layout className={styles.layout}>
+      <Header className={styles.header}>
+        <Icon
+          type="github"
+          component={() => <GithubFilled style={{ fontSize: 28 }} />}
+          style={{ color: 'white', marginRight: 16 }}
+        />
+        <Title style={{ color: 'white', fontSize: '28px', marginBottom: 0 }}>
+          Employee List
+        </Title>
+      </Header>
+
+      <Content className={styles.content}>{props.children}</Content>
+
+      <Footer className={styles.footer}>
+        <Text>© Anastasia Videneeva</Text>
+        <Link href="https://github.com/vi-nastya" target="_blank">
+          View source code
+        </Link>
+      </Footer>
+    </Layout>
+  )
+}
